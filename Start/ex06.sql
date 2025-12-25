3e3dcb5838461a0874ae12f957e701bd713f0bd9
kizdano <romreix24@gmail.com>
2025-12-25 23:15:03 +0300

    Update and rename ex6.sql to ex06.sql

---
+++ Start/ex06.sql
@@ -0,0 +1,11 @@
+SELECT 
+    (SELECT person.name 
+     FROM person 
+     WHERE person.id = person_order.person_id) AS NAME,
+     CASE 
+        WHEN (SELECT person.name FROM person WHERE person.id = person_order.person_id) = 'Ivan' THEN true
+        ELSE false
+    END AS check_name
+FROM person_order
+WHERE (person_order.menu_id = 13 OR person_order.menu_id = 14 OR person_order.menu_id = 18)
+  AND person_order.order_date = '2022-01-07'
