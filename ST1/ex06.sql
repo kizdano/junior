a6329aa55496ad3ec5ea1d731dbab1e3fe2c2b9b
kizdano <romreix24@gmail.com>
2025-12-25 23:18:13 +0300

    Create ex06.sql

---
+++ ST1/ex06.sql
@@ -0,0 +1,4 @@
+select order_date as action_date, (select name from person where id = person_order.person_id) as person_name from person_order
+union
+select visit_date as action_date, (select name from person where id = person_visits.person_id) as person_name from person_visits
+order by action_date asc, person_name desc
